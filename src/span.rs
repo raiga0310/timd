c2f6ce9fad1562d5fb71705d953f8b772a711b68
raiga0310 <raiga0310brilio@gmail.com>
2024-11-29 18:32:48 +0900

    feat: link parser

--- src/span.rs
+++ src/span.rs
@@ -1,7 +1,31 @@
+use combine::{between, many1, ParseError, Parser, Stream};
+use combine::parser::char::{char, letter};
+
+use crate::node::{Link, Node, Span, SpanType};
+
+pub fn link<Input>() -> impl Parser<Input, Output = Box<Node>>
+where
+    Input: Stream<Token = char>,
+    Input::Error: ParseError<Input::Token, Input::Range, Input::Position>,
+{
+    (
+        between(
+            char('['), 
+            char(']'), 
+            many1(letter())
+        ),
+        between(
+            char('('), 
+            char(')'), 
+            many1(letter())
+        ),
+    )
+        .map(|(display, link)| Span::new(SpanType::Link(Link::new(display, link)), vec![]))
+}
 
 #[cfg(test)]
 mod tests {
-    use crate::node::{Link, Span, Text};
+    use crate::node::{Link, Span};
 
     use super::*;
 
@@ -9,10 +33,14 @@ mod tests {
     fn parse_link() {
         assert_eq!(
             link().parse("[display](link)"),
-            Ok(Span::new(crate::node::SpanType::Link(Link::new(
-                "display".to_string(),
-                "link".to_string()
-            )), vec![]))
+            Ok((
+                Span::new(SpanType::Link(Link::new(
+                    "display".to_string(),
+                    "link".to_string()
+                )), 
+                vec![]), 
+                ""
+            ))
         )
     }
 }
