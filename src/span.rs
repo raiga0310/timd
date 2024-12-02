51a92734ff5f67c0707ca9f268a8ca6341db90f1
raiga0310 <raiga0310brilio@gmail.com>
2024-12-02 12:37:11 +0900

    feat: parser for strong

--- src/span.rs
+++ src/span.rs
@@ -3,6 +3,26 @@ use combine::{between, choice, many1, satisfy, ParseError, Parser, Stream};
 
 use crate::node::{Link, Node, Span, SpanType, Text};
 
+pub fn strong<Input>() -> impl Parser<Input, Output = Box<Node>>
+where 
+    Input: Stream<Token = char>,
+    Input::Error: ParseError<Input::Token, Input::Range, Input::Position>
+{
+    choice((
+        between(
+            (char('*'), char('*')),
+            (char('*'), char('*')),
+            parse_with_delimiter('*'), // temp
+        ),
+        between(
+            (char('_'), char('_')),
+            (char('_'), char('_')),
+            parse_with_delimiter('_'), // temp
+        ),
+    ))
+    .map(|content| Span::new(SpanType::Strong, vec![content]))
+}
+
 pub fn em<Input>() -> impl Parser<Input, Output = Box<Node>>
 where
     Input: Stream<Token = char>,
@@ -48,6 +68,28 @@ mod tests {
     use crate::node::{Link, Span};
 
     use super::*;
+
+    #[test]
+    fn parse_strong() {
+        {
+            assert_eq!(
+                strong().parse("**foo**"),
+                Ok((
+                    Span::new(SpanType::Strong, vec![Text::new("foo".to_string())]),
+                    ""
+                ))
+            )
+        }
+        {
+            assert_eq!(
+                strong().parse("__bar__"),
+                Ok((
+                    Span::new(SpanType::Strong, vec![Text::new("bar".to_string())]),
+                    ""
+                ))
+            )
+        }
+    }
 
     #[test]
     fn parse_em() {
