789b28b25bd4ac9dcb2268bd68ac52a79bbf1b78
raiga0310 <raiga0310brilio@gmail.com>
2024-12-02 12:38:00 +0900

    chore: fnt

--- src/span.rs
+++ src/span.rs
@@ -4,9 +4,9 @@ use combine::{between, choice, many1, satisfy, ParseError, Parser, Stream};
 use crate::node::{Link, Node, Span, SpanType, Text};
 
 pub fn strong<Input>() -> impl Parser<Input, Output = Box<Node>>
-where 
+where
     Input: Stream<Token = char>,
-    Input::Error: ParseError<Input::Token, Input::Range, Input::Position>
+    Input::Error: ParseError<Input::Token, Input::Range, Input::Position>,
 {
     choice((
         between(
